ff0931d2ebf40402c36a730d075d38c29bfc23f2
mdmellor <marcus.mellor@eagles.oc.edu>
2020-10-20 08:58:11 -0500

    Lab 10 Part 1

--- src/Thread.c
+++ src/Thread.c
@@ -8,6 +8,7 @@
 #include "stm32f4xx_hal.h"
 #include "stm32f4_discovery.h"
 #include "stm32f4_discovery_audio.h"
+#include "arm_math.h"
 #include <stdio.h>
 
 #define LED_Green   0
@@ -15,165 +16,67 @@
 #define LED_Red     2
 #define LED_Blue    3
 
-#define Show_Files_char "1"
-#define Set_File_char "4"
+#define NUM_CHAN	2 // number of audio channels
+#define NUM_POINTS 1024 // number of points per channel
+#define BUF_LEN NUM_CHAN*NUM_POINTS // length of the audio buffer
 
-enum commands {
-	ListFiles,
-	SendComplete,
-	SendFiles,
-	SetFile,
-};
+// Threads
+void Thread_1 (void const *argument); // Thread_1 prototype
+osThreadId tid_Thread_1;
+osThreadDef(Thread_1, osPriorityNormal, 1, 0);
 
-enum state {
-	NoState,
-	IdleState,
-	ListState,
-};
-
-//////////////////////////////////////////////////////////
-// Control thread
-void Control (void const *argument); // thread function
-osThreadId tid_Control; // thread id
-osThreadDef (Control, osPriorityNormal, 1, 0); // thread object
-
-void FS_Thread (void const *argument);
-osThreadId tid_FS_Thread;
-osThreadDef (FS_Thread, osPriorityNormal, 1, 0);
-
-// UART receive thread
-void Rx_Command (void const *argument);  // thread function
-osThreadId tid_RX_Command;  // thread id
-osThreadDef (Rx_Command, osPriorityNormal, 1, 0); // thread object
-
-// Command queue from Rx_Command to Control
-osMessageQId mid_CMDQueue; // message queue for commands to Thread
-osMessageQDef (CMDQueue, 1, uint32_t); // message queue object
-
-// Command queue from Control to FS_Thread
-osMessageQId comman_FSQueue;
-osMessageQDef (FSQueue, 1, uint32_t);
-
-void Process_Event(uint16_t event){
-	static uint16_t   Current_State = NoState; // Current state of the SM
-	switch(Current_State) {
-    	case NoState:
-    		// Next State
-    		Current_State = IdleState;
-    		// Exit actions
-    		// Transition actions
-    		// IdleState entry actions
-    		LED_On(LED_Red);
-    		break;
-    	case IdleState:
-    		if (event == ListFiles) {
-    			// Next State
-    			Current_State = ListState;
-    			// Exit Actions
-    			LED_Off(LED_Red);
-    			// Transition actions
-    			// ListState Entry actions
-    			LED_On(LED_Blue);
-    			osMessagePut(comman_FSQueue, SendFiles, osWaitForever);
-    		}
-    		break;
-    	case ListState:
-    		if (event == SendComplete) {
-    			// Next State
-    			Current_State = IdleState;
-    			// Exit Actions
-    			LED_Off(LED_Blue);
-    			// Transition Actions
-    			// IdleState Entry actions
-    			LED_On(LED_Red);
-    		}
-    		break;
-    	default:
-    		break;
-	} // end case(Current_State)
-} // Process_Event
-
+// buffer used for audio play
+int16_t Audio_Buffer[BUF_LEN];
 
 void Init_Thread (void) {
 	LED_Initialize(); // Initialize the LEDs
 	UART_Init(); // Initialize the UART
 	// Create queues
-	mid_CMDQueue = osMessageCreate (osMessageQ(CMDQueue), NULL);  // create msg queue
-	if (!mid_CMDQueue) return; // Message Queue object not created, handle failure
-	comman_FSQueue = osMessageCreate (osMessageQ(FSQueue), NULL);
-	if (!comman_FSQueue) return;
+
 	// Create threads
-	tid_Control = osThreadCreate (osThread(Control), NULL);
-	if (!tid_Control) return;
-	tid_FS_Thread = osThreadCreate(osThread(FS_Thread), NULL);
-	if (!tid_FS_Thread) return;
-	tid_RX_Command = osThreadCreate (osThread(Rx_Command), NULL);
-	if (!tid_RX_Command) return;
+	tid_Thread_1 = osThreadCreate(osThread(Thread_1), NULL);
+	if (!tid_Thread_1) return;
 }
 
-void Control (void const *argument) {
-	osEvent evt;
-	Process_Event(0);
-	while (true) {
-		evt = osMessageGet(mid_CMDQueue, osWaitForever);
-		if (evt.status == osEventMessage) {
-			Process_Event(evt.value.v);
-		}
+void Thread_1 (void const *argument) {
+	static uint8_t rtrn = 0;  	// return variable
+	float32_t Fs = 8000.0; 		// sample frequency
+	float32_t freq = 250.0; 	// signal frequency in Hz
+	int16_t i; 					// loop counter
+	float32_t tmp; 				// factor
+	float32_t cnt = 0.0f; 		// time index
+
+	tmp = 6.28f*freq/Fs; // only calc this factor once
+
+	// initialize the audio output
+	rtrn = BSP_AUDIO_OUT_Init(OUTPUT_DEVICE_AUTO, 0x46, 8000);
+	if (rtrn != AUDIO_OK) return;
+
+	// generate data for the audio buffer
+ 	for (i=0;i<NUM_POINTS;i++) {
+		Audio_Buffer[2*i] = arm_cos_f32(tmp*cnt)*32000.0f; // Left channel
+		Audio_Buffer[2*i+1] = Audio_Buffer[2*i]; // Right channel
+		cnt = cnt + 1.0f;
 	}
+	// Start the audio player, size is number of bytes so mult. by 2
+ 	BSP_AUDIO_OUT_Play((uint16_t *) Audio_Buffer, BUF_LEN*2);
 }
 
-void FS_Thread (void const *argument) {
-	osEvent evt;
-	usbStatus ustatus;
-	uint8_t drivenum = 0;
-	char *drive_name = "U0";
-	fsStatus fstatus;
-	fsFileInfo info;
-	ustatus = USBH_Initialize(drivenum);
-	if (ustatus == usbOK) {
-		ustatus = USBH_Device_GetStatus(drivenum);
-		while (ustatus != usbOK) {
-			ustatus = USBH_Device_GetStatus(drivenum);
-		}
-		fstatus = finit(drive_name);
-		if (fstatus != fsOK) {
-			return;
-		}
-		fstatus = fmount(drive_name);
-		if (fstatus != fsOK) {
-			return;
-		}
-		while (true) {
-			evt = osMessageGet(comman_FSQueue, osWaitForever);
-			if (evt.status == osEventMessage && evt.value.v == SendFiles) {
-				char *StartFileList_msg = "2\n";
-				char *EndFileList_msg = "3\n";
-				char *file_buffer;
-				UART_send(StartFileList_msg, 2);
-				info.fileID = 0;
-				while (ffind("*.txt", &info) == fsOK) {
-					file_buffer = info.name;
-					UART_send(file_buffer, strlen(file_buffer));
-					UART_send("\n", 1);
-				}
-				UART_send(EndFileList_msg, 2);
-				osMessagePut(mid_CMDQueue, SendComplete, osWaitForever);
-			}
-		}
-	}
+/* User Callbacks: user has to implement these functions if they are needed. */
+/* This function is called when the requested data has been completely transferred. */
+void BSP_AUDIO_OUT_TransferComplete_CallBack (void) {
+	BSP_AUDIO_OUT_ChangeBuffer((uint16_t*) Audio_Buffer, BUF_LEN);
 }
 
-void Rx_Command (void const *argument) {
-	char rx_char[2] = {0,0};
-	while (true) {
-		UART_receive(rx_char, 1);
-		if (!strcmp(rx_char, Show_Files_char)) {
-			osMessagePut(mid_CMDQueue, ListFiles, osWaitForever);
-		}
-		else if (!strcmp(rx_char, Set_File_char)) {
-			char selectedFileName[20];
-			UART_receivestring(selectedFileName, 20);
-			char *_ = selectedFileName;
-		}
-	}
+/* This function is called when half of the requested buffer has been transferred. */
+void BSP_AUDIO_OUT_HalfTransfer_CallBack (void) {
+	// pass
 }
+
+/* This function is called when an Interrupt due to transfer error or peripheral
+   error occurs. */
+void BSP_AUDIO_OUT_Error_CallBack(void){
+	while (1) { }
+}
+
+
